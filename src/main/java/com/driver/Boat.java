99c4dbe5e2bb87842727c290c2b659f1cda269d5
VishalSinghShekhawat <vss1664883@gmail.com>
2024-05-29 10:01:24 +0530

    First Attempt

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -6,6 +6,14 @@ public class Boat implements WaterVehicle{
 
     private int capacity;
 
+    public Boat() {
+    }
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     @Override
     public String getVehicleName() {
         return name;
@@ -14,5 +22,21 @@ public class Boat implements WaterVehicle{
     @Override
     public int getVehicleCapacity() {
         return capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
     }
 }
